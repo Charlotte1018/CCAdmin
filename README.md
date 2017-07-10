581d70f43053db25f1acbd40f6b1080514fa55b5
时昌雪 <925290164@qq.com>
2017-07-10 15:42:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# CCAdmin
+
+## install from sourceCode:
+
+* step 1: 
+get code from github: https://github.com/stevenkcolin/acicoadmin
+
+* step 2: `npm i` to install node_modules
+
+* step 3: `bower install` to install bower components
+
+* step 4: `npm run build`;
+
+* step 4: `npm run dev` to run this webAdmin
+
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var htmlWebpackPlugin = require("html-webpack-plugin");
+var cleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './app/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/[name].js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './app/views/**/*', to: '' }
+    ]),
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      inject: false,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve("app"),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|woff2|woff|ttf)$/i,
+        loader: "file-loader"
+      }
+    ]
+  }
+}
